7e62f05961673c1d873180421f99268dbea4836b
ssamuyy <samu.yrttiaho@student.lut.fi>
2025-04-06 21:37:34 +0200

    Hirviöpeli v1.1.231

--- app/src/main/java/com/main/v12t1/MainActivity.java
+++ app/src/main/java/com/main/v12t1/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button fightMonsterButton = findViewById(R.id.FightMonsterActivityButton);
+        Button fightMonsterButton = findViewById(R.id.FightMonstersActivityButton);
         scoreText = findViewById(R.id.PlayerScoreText);
 
         fightMonsterButton.setOnClickListener(v -> {
